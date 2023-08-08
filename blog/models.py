050cf8957261b39a2c5b6f2a3870bb8bd650be5d
Luis Castañeda <luis.ecs@hotmail.com>
2023-08-07 22:51:03 -0600

    make migrations and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
-class Articulo (models.Model):
-     titulo =
-     subtitulo =
-     cuerpo =
-     autor =
-     fecha = 
-#     imagen =
+
+class Post(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now) #fecha
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    class Meta:
+        ordering = ['-timestamp']
+    
+    def __str__(self):
+        return self.content
--- perfiles/models.py
+++ perfiles/models.py
@@ -1,16 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    username = 
-    email =
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=True)
     bio = models.CharField(default='Hola, Blogger', max_length=100)
-    image =
+    # image = models.ImageField(default='default.png')
+
+    def __str__(self):
+        return f'Perfil de {self.username}' #{self.user.username}
 
 
-
-class Post(model.Model):
-    pass
-
-
-#register va en views
