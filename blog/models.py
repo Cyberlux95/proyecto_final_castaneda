b8767e0dfcb51ed42c219c892db6774effd0a064
Luis Castañeda <luis.ecs@hotmail.com>
2023-08-07 22:21:03 -0600

    inicio de models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Articulo (models.Model):
+     titulo =
+     subtitulo =
+     cuerpo =
+     autor =
+     fecha = 
+#     imagen =
--- perfiles/models.py
+++ perfiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    username = 
+    email =
+    bio = models.CharField(default='Hola, Blogger', max_length=100)
+    image =
+
+
+
+class Post(model.Model):
+    pass
+
+
+#register va en views
